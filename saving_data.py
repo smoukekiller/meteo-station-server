040dfbde7053093d44cbfa62eb43c59fe35aca01
smoukekiller <4406248@gmail.com>
2025-01-04 22:35:48 +0200

    added config file

--- saving_data.py
+++ saving_data.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-
+import config
 def save_data(temperature: float, humidity: float) -> None:
     ct = datetime.now()
     timestamp = int(round(ct.timestamp()))
     filename = ct.isoformat()[:10]
-    data_file = open(f"{filename}.txt", "a+")
+    data_file = open(f"{config.DATA_PATH}{filename}.txt", "a+")
     data_file.write(str(timestamp) + ";" + str(temperature) + ";" + str(humidity) + "\n")
     data_file.close()
     
